a0bbaedaf79fe1fc175abb9d4ce1881754ed8de0
theapache64 <theapache64@users.noreply.github.com>
2022-11-28 01:24:07 +0530

    🎉 prepare release : 1.0.0-alpha04

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     id("org.jetbrains.compose") version "1.2.1"
 }
 group = "com.theapache64.benchart"
-version = "1.0.0-alpha03"
+version = "1.0.0-alpha04"
 
 // Add maven repositories
 repositories {
